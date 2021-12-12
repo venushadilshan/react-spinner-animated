5e9b5afbeaf9d46eaef8a1fc8eff84d3ca06e20a
Venusha Dilshan Dushmantha <venusha125@gmail.com>
2021-12-12 17:02:31 +0530

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const BarLoader = require('./components/BarLoader/BarLoader');
+const DoubleBubble = require('/components/BarLoader/DoubleBubble');
+const Spinner = require('./components/BarLoader/Spinner');
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+module.exports = {
+    BarLoader,Spinner,DoubleBubble
+}
